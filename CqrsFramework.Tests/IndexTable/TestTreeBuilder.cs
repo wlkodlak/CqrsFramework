5c15e7a3ffc1ed9332ab2a79d90aab9313b02e9b
Milan Wilczak <wlkodlak@seznam.cz>
2013-05-25 19:50:45 +0200

    Generovani stromu pro testy

---
+++ CqrsFramework.Tests/IndexTable/TestTreeBuilder.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsFramework.IndexTable;
+
+namespace CqrsFramework.Tests.IndexTable
+{
+    public class TestTreeBuilder
+    {
+        private List<TestTreeNodeBuilder> _nodeBuilders = new List<TestTreeNodeBuilder>();
+        private Random _random = new Random(48324);
+        private Dictionary<string, byte[]> _namedValues = new Dictionary<string, byte[]>();
+
+        private Dictionary<int, IIdxPage> _pagesIndex = new Dictionary<int, IIdxPage>();
+        private List<IIdxPage> _pages = new List<IIdxPage>();
+
+        public void Build()
+        {
+            foreach (var node in _nodeBuilders.ToList())
+            {
+                if (node.IsLeaf)
+                    PrepareLeafNode(node);
+                else
+                    PrepareInteriorNode(node);
+            }
+            var roots = new List<TestTreeNodeBuilder>();
+            foreach (var node in _nodeBuilders.ToList())
+            {
+                AllocPageNumbersForNode(node);
+                if (node.ParentNode == null)
+                    roots.Add(node);
+            }
+            foreach (var root in roots)
+                root.DistributeNext(null);
+            foreach (var node in _nodeBuilders.ToList())
+            {
+                if (node.IsLeaf)
+                    node.LeafPage.NextLeaf = node.NextNode == null ? 0 : node.NextNode.PageNumber;
+                else
+                {
+                    foreach (var cell in node.Cells)
+                    {
+                        var page = cell.Node == null ? AllocPage() : cell.Node.PageNumber;
+                        cell.Cell = IdxCell.CreateInteriorCell(cell.Key, page, PageSize);
+                    }
+                }
+            }
+            foreach (var page in _pages)
+                _pagesIndex[page.PageNumber] = page;
+        }
+
+        private void AllocPageNumbersForNode(TestTreeNodeBuilder node)
+        {
+            if (node.PageNumber == 0)
+                node.PageNumber = AllocPage();
+            if (node.IsLeaf)
+            {
+                node.LeafPage.PageNumber = node.PageNumber;
+                foreach (var cell in node.Cells)
+                {
+                    for (int i = cell.OverflowPages.Count - 1; i >= 0; i--)
+                    {
+                        int page = cell.OverflowPages[i].PageNumber;
+                        if (page == 0)
+                            cell.OverflowPages[i].PageNumber = page = AllocPage();
+                        if (i == 0)
+                            cell.Cell.OverflowPage = page;
+                        else
+                            cell.OverflowPages[i - 1].Next = page;
+                    }
+                }
+            }
+            else
+                node.InteriorPage.PageNumber = node.PageNumber;
+        }
+
+        private void PrepareLeafNode(TestTreeNodeBuilder node)
+        {
+            node.LeafPage = new IdxLeaf(null, PageSize);
+            node.LeafPage.PageNumber = node.PageNumber;
+            MarkAsUsed(node.PageNumber);
+            _pages.Add(node.LeafPage);
+            foreach (var cellBuilder in node.Cells)
+            {
+                PrepareLeafCell(cellBuilder);
+            }
+        }
+
+        private void PrepareLeafCell(TestTreeCellBuilder cellBuilder)
+        {
+            cellBuilder.Cell = IdxCell.CreateLeafCell(cellBuilder.Key, cellBuilder.Value, PageSize);
+            var offset = cellBuilder.Cell.ValueLength;
+            var remaining = (cellBuilder.Value == null) ? 0 : cellBuilder.Value.Length - cellBuilder.Cell.ValueLength;
+            while (remaining > 0)
+            {
+                var overflowPage = new IdxOverflow(null, PageSize);
+                var written = overflowPage.WriteData(cellBuilder.Value, offset);
+                offset += written;
+                remaining -= written;
+                cellBuilder.OverflowPages.Add(overflowPage);
+                _pages.Add(overflowPage);
+            }
+            if (cellBuilder.OverflowPageNumbers != null)
+            {
+                for (int i = cellBuilder.OverflowPages.Count; i < cellBuilder.OverflowPageNumbers.Length; i++)
+                {
+                    var overflowPage = new IdxOverflow(null, PageSize);
+                    _pages.Add(overflowPage);
+                    cellBuilder.OverflowPages.Add(overflowPage);
+                }
+                for (int i = 0; i < cellBuilder.OverflowPageNumbers.Length; i++)
+                {
+                    cellBuilder.OverflowPages[i].PageNumber = cellBuilder.OverflowPageNumbers[i];
+                    MarkAsUsed(cellBuilder.OverflowPageNumbers[i]);
+                }
+            }
+        }
+
+        private void PrepareInteriorNode(TestTreeNodeBuilder node)
+        {
+            node.InteriorPage = new IdxInterior(null, PageSize);
+            node.InteriorPage.PageNumber = node.PageNumber;
+            MarkAsUsed(node.PageNumber);
+            _pages.Add(node.InteriorPage);
+            if (node.Leftmost == null)
+                node.Leftmost = BuildChildNodeForInterior(node);
+            node.Leftmost.ParentNode = node;
+            foreach (var cellBuilder in node.Cells)
+            {
+                if (cellBuilder.Node == null)
+                    cellBuilder.Node = BuildChildNodeForInterior(node);
+                cellBuilder.Node.ParentNode = node;
+            }
+        }
+
+        private TestTreeNodeBuilder BuildChildNodeForInterior(TestTreeNodeBuilder node)
+        {
+            var newNode = new TestTreeNodeBuilder(this);
+            newNode.IsLeaf = node.ContentsType != 1;
+            _nodeBuilders.Add(newNode);
+            if (newNode.IsLeaf)
+                PrepareLeafNode(newNode);
+            else
+                PrepareInteriorNode(newNode);
+            return newNode;
+        }
+
+        public int PageSize;
+        public int MinKeySize;
+
+        private int _pageNumber = 2;
+        private List<int> _usablePages = new List<int>();
+
+        private void MarkAsUsed(int page)
+        {
+            if (page == 0)
+                return;
+            if (page < _pageNumber)
+                _usablePages.Remove(page);
+            else if (page == _pageNumber)
+                _pageNumber++;
+            else
+            {
+                for (int i = _pageNumber; i < page; i++)
+                    _usablePages.Add(i);
+                _pageNumber = page + 1;
+            }
+        }
+
+        private int AllocPage()
+        {
+            if (_usablePages.Count == 0)
+            {
+                int page = _pageNumber;
+                _pageNumber++;
+                return page;
+            }
+            else
+            {
+                int index = _usablePages.Count - 1;
+                int page = _usablePages[index];
+                _usablePages.RemoveAt(index);
+                return page;
+            }
+        }
+
+        public TestTreeNodeBuilder Interior(int pageNumber)
+        {
+            var node = new TestTreeNodeBuilder(this);
+            node.IsLeaf = false;
+            node.PageNumber = pageNumber;
+            _nodeBuilders.Add(node);
+            return node;
+        }
+
+        public TestTreeNodeBuilder Leaf(int pageNumber)
+        {
+            var node = new TestTreeNodeBuilder(this);
+            node.IsLeaf = true;
+            node.PageNumber = pageNumber;
+            _nodeBuilders.Add(node);
+            return node;
+        }
+
+        public void SetNamedValue(string name, byte[] value)
+        {
+            _namedValues[name] = value;
+        }
+
+        public byte[] GetNamedValue(string name)
+        {
+            byte[] output;
+            if (_namedValues.TryGetValue(name, out output))
+                return output;
+            else
+                return new byte[0];
+        }
+
+        public IIdxNode GetNode(int page)
+        {
+            IIdxPage contents;
+            if (!_pagesIndex.TryGetValue(page, out contents))
+                throw new KeyNotFoundException(string.Format("Page {0} not found", page));
+            return (IIdxNode)contents;
+        }
+
+        public IdxOverflow GetOverflow(int page)
+        {
+            IIdxPage contents;
+            if (!_pagesIndex.TryGetValue(page, out contents))
+                throw new KeyNotFoundException(string.Format("Page {0} not found", page));
+            return (IdxOverflow)contents;
+        }
+
+        public byte[] CreateValue(int length)
+        {
+            byte[] value = new byte[length];
+            _random.NextBytes(value);
+            return value;
+        }
+
+        public IdxKey BuildKey(int keyBase)
+        {
+            var intKey = IdxKey.FromInteger(keyBase);
+            var intKeyBytes = intKey.ToBytes();
+            if (MinKeySize <= intKeyBytes.Length)
+                return intKey;
+            var longKey = new byte[MinKeySize];
+            Array.Copy(intKeyBytes, longKey, intKeyBytes.Length);
+            return IdxKey.FromBytes(longKey);
+        }
+
+        public TestTreeCellBuilder LongCell(int keyBase, byte[] value, params int[] overflowPages)
+        {
+            var cell = new TestTreeCellBuilder();
+            cell.Key = BuildKey(keyBase);
+            cell.Value = value;
+            cell.OverflowPageNumbers = overflowPages;
+            return cell;
+        }
+    }
+
+    public class TestTreeNodeBuilder
+    {
+        private TestTreeBuilder _builder;
+        public bool IsLeaf;
+        public int PageNumber;
+        public TestTreeNodeBuilder Leftmost;
+        public List<TestTreeCellBuilder> Cells = new List<TestTreeCellBuilder>();
+        private TestTreeCellBuilder _currentCell;
+        public IdxLeaf LeafPage;
+        public IdxInterior InteriorPage;
+        public TestTreeNodeBuilder ParentNode;
+        public TestTreeNodeBuilder NextNode;
+        public int ContentsType = 0;
+
+        public TestTreeNodeBuilder(TestTreeBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void MarkAsAboveLeaves()
+        {
+            ContentsType = 2;
+        }
+
+        public void MarkAsAboveInteriors()
+        {
+            ContentsType = 1;
+        }
+        
+        public void AddContents(params object[] contents)
+        {
+            if (!IsLeaf)
+            {
+                foreach (object member in contents)
+                {
+                    if (member is TestTreeNodeBuilder)
+                    {
+                        var nodeBuilder = member as TestTreeNodeBuilder;
+                        if (_currentCell == null)
+                            Leftmost = nodeBuilder;
+                        else if (_currentCell.Node == null)
+                            _currentCell.Node = nodeBuilder;
+                        ContentsType = nodeBuilder.IsLeaf ? 2 : 1;
+                    }
+                    else if (member is int)
+                    {
+                        _currentCell = new TestTreeCellBuilder();
+                        _currentCell.Key = _builder.BuildKey((int)member);
+                        _currentCell.IsLeaf = false;
+                        Cells.Add(_currentCell);
+                    }
+                }
+            }
+            else
+            {
+                foreach (object member in contents)
+                {
+                    if (member is int)
+                    {
+                        _currentCell = new TestTreeCellBuilder();
+                        _currentCell.Key = _builder.BuildKey((int)member);
+                        _currentCell.IsLeaf = true;
+                        Cells.Add(_currentCell);
+                    }
+                    else if (member is byte[])
+                        _currentCell.Value = member as byte[];
+                    else if (member is TestTreeCellBuilder)
+                    {
+                        _currentCell = member as TestTreeCellBuilder;
+                        Cells.Add(_currentCell);
+                    }
+                }
+            }
+        }
+
+        public TestTreeNodeBuilder DistributeNext(TestTreeNodeBuilder nextNode)
+        {
+            if (IsLeaf)
+            {
+                NextNode = nextNode;
+                return this;
+            }
+            else
+            {
+                for (int i = Cells.Count - 1; i >= 0; i--)
+                    nextNode = Cells[i].Node.DistributeNext(nextNode);
+                return nextNode;
+            }
+        }
+    }
+
+    public class TestTreeCellBuilder
+    {
+        public IdxKey Key;
+        public bool IsLeaf;
+        public TestTreeNodeBuilder Node;
+        public byte[] Value;
+        public int[] OverflowPageNumbers;
+        public IdxCell Cell;
+        public List<IdxOverflow> OverflowPages = new List<IdxOverflow>();
+    }
+}
--- CqrsFramework.Tests/IndexTable/TreeReadTest.cs
+++ CqrsFramework.Tests/IndexTable/TreeReadTest.cs
@@ -25,7 +25,7 @@ namespace CqrsFramework.Tests.IndexTable
         [TestMethod]
         public void EmptyTree()
         {
-            var leaf = new IdxLeaf(null, 4096);
+            var leaf = new IdxLeaf(null, 512);
             leaf.PageNumber = 47;
             leaf.Save();
 
@@ -43,13 +43,13 @@ namespace CqrsFramework.Tests.IndexTable
         public void SingleLeafWithShortValuesButNoResult()
         {
             var random = new Random(84324);
-            var root = new IdxLeaf(null, 4096);
+            var root = new IdxLeaf(null, 512);
             root.PageNumber = 47;
             root.NextLeaf = 0;
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(2), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(4), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(9), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(17), ContainerTestUtilities.CreateBytes(random, 84), 4096));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(2), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(4), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(9), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(17), ContainerTestUtilities.CreateBytes(random, 84), 512));
             root.Save();
 
             var mock = new Mock<IIdxContainer>();
@@ -66,13 +66,13 @@ namespace CqrsFramework.Tests.IndexTable
         public void SingleLeafWithShortValuesWithResults()
         {
             var random = new Random(84324);
-            var root = new IdxLeaf(null, 4096);
+            var root = new IdxLeaf(null, 512);
             root.PageNumber = 47;
             root.NextLeaf = 0;
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(2), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(5), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(7), ContainerTestUtilities.CreateBytes(random, 84), 4096));
-            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(9), ContainerTestUtilities.CreateBytes(random, 84), 4096));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(2), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(5), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(7), ContainerTestUtilities.CreateBytes(random, 84), 512));
+            root.AddCell(IdxCell.CreateLeafCell(IdxKey.FromInteger(9), ContainerTestUtilities.CreateBytes(random, 84), 512));
             root.Save();
 
             var mock = new Mock<IIdxContainer>();
@@ -87,6 +87,102 @@ namespace CqrsFramework.Tests.IndexTable
                 .Select(c => new KeyValuePair<IdxKey, byte[]>(c.Key, c.ValueBytes))
                 .ToList();
             CollectionAssert.AreEqual(expectedResult, selected, "Result items");
+            mock.Verify();
+        }
+
+        private TestTreeBuilder CreateComplexTree()
+        {
+            var builder = new TestTreeBuilder();
+            builder.PageSize = 1024;
+            builder.MinKeySize = 120;
+            builder.SetNamedValue("key51.value", builder.CreateValue(2744));
+            var root = builder.Interior(2);
+            var lvl1 = builder.Interior(3);
+            var lvl2a = builder.Leaf(4);
+            var lvl2b = builder.Leaf(5);
+            var lvl2c = builder.Leaf(6);
+            var lvl2d = builder.Leaf(7);
+            root.AddContents(20, 50, lvl1, 80, 100);
+            lvl1.AddContents(50, lvl2a, 54, lvl2b, 60, lvl2c, 68, lvl2d, 75);
+            lvl2a.AddContents(50, builder.LongCell(51, builder.GetNamedValue("key51.value"), 10, 11));
+            lvl2b.AddContents(54, 56, 57, 58);
+            lvl2c.AddContents(60, 62, 63);
+            lvl2d.AddContents(68, 69, 70, 72, 74);
+            builder.Build();
+            return builder;
+        }
+
+        [TestMethod]
+        public void SearchForNonexistentKey()
+        {
+            var builder = CreateComplexTree();
+            var mock = new Mock<IIdxContainer>();
+            mock.Setup(c => c.ReadTree(0)).Returns(builder.GetNode(2)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 3)).Returns(builder.GetNode(3)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 5)).Returns(builder.GetNode(5)).Verifiable();
+            mock.Setup(c => c.UnlockRead(0));
+
+            IdxTree tree = new IdxTree(mock.Object, 0);
+            var selected = tree.Select(builder.BuildKey(55), builder.BuildKey(55));
+            Assert.AreEqual(0, selected.Count());
+            mock.Verify();
+        }
+
+        [TestMethod]
+        public void SearchForRangeInSingleNode()
+        {
+            var builder = CreateComplexTree();
+            var mock = new Mock<IIdxContainer>();
+            mock.Setup(c => c.ReadTree(0)).Returns(builder.GetNode(2)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 3)).Returns(builder.GetNode(3)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 5)).Returns(builder.GetNode(5)).Verifiable();
+            mock.Setup(c => c.UnlockRead(0));
+
+            IdxTree tree = new IdxTree(mock.Object, 0);
+            var selected = tree.Select(builder.BuildKey(56), builder.BuildKey(58)).ToList();
+            Assert.AreEqual(3, selected.Count, "Count");
+            Assert.AreEqual(builder.BuildKey(56), selected[0].Key, "Key 56");
+            Assert.AreEqual(builder.BuildKey(57), selected[1].Key, "Key 57");
+            Assert.AreEqual(builder.BuildKey(58), selected[2].Key, "Key 58");
+            mock.Verify();
+        }
+
+        [TestMethod]
+        public void SearchForRangeAcrossNodes()
+        {
+            var builder = CreateComplexTree();
+            var mock = new Mock<IIdxContainer>();
+            mock.Setup(c => c.ReadTree(0)).Returns(builder.GetNode(2)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 3)).Returns(builder.GetNode(3)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 5)).Returns(builder.GetNode(5)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 6)).Returns(builder.GetNode(6)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 7)).Returns(builder.GetNode(7)).Verifiable();
+            mock.Setup(c => c.UnlockRead(0));
+            var tree = new IdxTree(mock.Object, 0);
+            var selected = tree.Select(builder.BuildKey(55), builder.BuildKey(71)).ToList();
+            var keyBases = new int[] { 56, 57, 58, 60, 62, 63, 68, 69, 70 };
+            Assert.AreEqual(keyBases.Length, selected.Count, "Count");
+            for (int i = 0; i < keyBases.Length; i++)
+                Assert.AreEqual(builder.BuildKey(keyBases[i]), selected[i], "Key {0}", keyBases[i]);
+            mock.Verify();
+        }
+
+        [TestMethod]
+        public void LoadOverflowData()
+        {
+            var builder = CreateComplexTree();
+            var mock = new Mock<IIdxContainer>();
+            mock.Setup(c => c.ReadTree(0)).Returns(builder.GetNode(2)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 3)).Returns(builder.GetNode(3)).Verifiable();
+            mock.Setup(c => c.GetNode(0, 4)).Returns(builder.GetNode(4)).Verifiable();
+            mock.Setup(c => c.GetOverflow(0, 10)).Returns(builder.GetOverflow(10)).Verifiable();
+            mock.Setup(c => c.GetOverflow(0, 11)).Returns(builder.GetOverflow(11)).Verifiable();
+            mock.Setup(c => c.UnlockRead(0));
+            var tree = new IdxTree(mock.Object, 0);
+            var selected = tree.Select(builder.BuildKey(51), builder.BuildKey(51)).FirstOrDefault();
+            Assert.IsNotNull(selected, "Found");
+            Assert.AreEqual(builder.BuildKey(51), selected.Key, "Key");
+            CollectionAssert.AreEqual(builder.GetNamedValue("key51.value"), selected.Value, "Contents");
             mock.Verify();
         }
     }
