d41111771cb7849c4173be469af5f5cadaf9887a
Milan Wilczak <wlkodlak@seznam.cz>
2013-06-16 14:24:28 +0200

    Jadro indexu pro FileEventStore

---
+++ CqrsFramework.Tests/EventStore/EventStoreFilesIndexFileTest.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsFramework.IndexTable;
+using CqrsFramework.EventStore;
+using Moq;
+
+namespace CqrsFramework.Tests.EventStore
+{
+    [TestClass]
+    public class EventStoreFilesIndexFileTest
+    {
+        private Mock<IIdxTree> _streams;
+        private Mock<IIdxTree> _headers;
+
+        private IdxKey _appendPositionKey = IdxKey.FromBytes(new byte[1] { 0 });
+        private IdxKey _unpublishedPositionKey = IdxKey.FromBytes(new byte[1] { 1 });
+        private byte[] _appendPositionBytes;
+        private byte[] _unpublishedPositionBytes;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _headers = new Mock<IIdxTree>(MockBehavior.Strict);
+            _streams = new Mock<IIdxTree>(MockBehavior.Strict);
+
+            _headers
+                .Setup(h => h.Select(It.IsAny<IdxKey>(), It.IsAny<IdxKey>()))
+                .Returns<IdxKey, IdxKey>(SelectFromHeaders);
+            _headers
+                .Setup(h => h.Insert(_appendPositionKey, It.IsAny<byte[]>()))
+                .Callback<IdxKey, byte[]>((k, v) => SetHeaderValue(1, v, false));
+            _headers
+                .Setup(h => h.Insert(_unpublishedPositionKey, It.IsAny<byte[]>()))
+                .Callback<IdxKey, byte[]>((k, v) => SetHeaderValue(2, v, false));
+            _headers
+                .Setup(h => h.Update(_appendPositionKey, It.IsAny<byte[]>()))
+                .Callback<IdxKey, byte[]>((k, v) => SetHeaderValue(1, v, true));
+            _headers
+                .Setup(h => h.Update(_unpublishedPositionKey, It.IsAny<byte[]>()))
+                .Callback<IdxKey, byte[]>((k, v) => SetHeaderValue(2, v, true));
+        }
+
+        private IEnumerable<KeyValuePair<IdxKey, byte[]>> SelectFromHeaders(IdxKey min, IdxKey max)
+        {
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            if (_appendPositionBytes != null)
+                result.Add(new KeyValuePair<IdxKey, byte[]>(_appendPositionKey, _appendPositionBytes));
+            if (_unpublishedPositionBytes != null)
+                result.Add(new KeyValuePair<IdxKey, byte[]>(_unpublishedPositionKey, _unpublishedPositionBytes));
+            return result.Where(i => min <= i.Key && i.Key <= max).ToList();
+        }
+
+        private void SetHeaderValue(int key, byte[] value, bool isUpdate)
+        {
+            var keyName = new string[] { "", "append", "unpublished" };
+            var origValue = new byte[][] { null, _appendPositionBytes, _unpublishedPositionBytes };
+            Assert.IsNotNull(value, "Set to null {0} bytes", keyName[key]);
+            if (isUpdate)
+                Assert.IsNotNull(origValue[key], "Null {0} bytes", keyName[key]);
+            else
+                Assert.IsNull(origValue[key], "Null {0} bytes", keyName[key]);
+            if (key == 1)
+                _appendPositionBytes = value;
+            else if (key == 2)
+                _unpublishedPositionBytes = value;
+        }
+
+        private byte[] LongToBytes(long value)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return bytes;
+        }
+
+        private IdxKey StreamKey(string streamId, bool snapshot, int version)
+        {
+            var stringBytes = Encoding.ASCII.GetBytes(streamId);
+            var lengthBytes = BitConverter.GetBytes((short)stringBytes.Length);
+            var typeBytes = new byte[1] { snapshot ? (byte)0 : (byte)1 };
+            var versionBytes = BitConverter.GetBytes(version);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthBytes);
+                Array.Reverse(versionBytes);
+            }
+            var bytes = lengthBytes.Concat(stringBytes).Concat(typeBytes).Concat(versionBytes).ToArray();
+            return IdxKey.FromBytes(bytes);
+        }
+
+        [TestMethod]
+        public void GetPositionsInEmpty()
+        {
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            Assert.AreEqual(0, index.UnpublishedPosition);
+            Assert.AreEqual(0, index.AppendPosition);
+        }
+
+        [TestMethod]
+        public void SetPositionsInEmpty()
+        {
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            index.AppendPosition = 472235447;
+            index.UnpublishedPosition = 2512745;
+            index.Flush();
+            AssertExtension.AreEqual(LongToBytes(472235447), _appendPositionBytes, "Append position");
+            AssertExtension.AreEqual(LongToBytes(2512745), _unpublishedPositionBytes, "Unpublished position");
+        }
+
+        [TestMethod]
+        public void GetPositionsInUsed()
+        {
+            _appendPositionBytes = LongToBytes(544);
+            _unpublishedPositionBytes = LongToBytes(333);
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            Assert.AreEqual(333, index.UnpublishedPosition);
+            Assert.AreEqual(544, index.AppendPosition);
+        }
+
+        [TestMethod]
+        public void SetPositionsInUsed()
+        {
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            index.AppendPosition = 472235447;
+            index.UnpublishedPosition = 2512745;
+            index.Flush();
+            AssertExtension.AreEqual(LongToBytes(472235447), _appendPositionBytes, "Append position");
+            AssertExtension.AreEqual(LongToBytes(2512745), _unpublishedPositionBytes, "Unpublished position");
+        }
+
+        [TestMethod]
+        public void AddRecord()
+        {
+            _streams.Setup(s => s.Insert(It.IsAny<IdxKey>(), It.IsAny<byte[]>())).Callback<IdxKey, byte[]>((k, v) =>
+                {
+                    Assert.AreEqual(StreamKey("Aggregate:111", false, 14), k, "Key");
+                    AssertExtension.AreEqual(LongToBytes(85447), v, "Value");
+                }).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            index.AddRecord("Aggregate:111", 14, 85447);
+            _streams.Verify();
+        }
+
+        [TestMethod]
+        public void AddSnapshot()
+        {
+            _streams.Setup(s => s.Insert(It.IsAny<IdxKey>(), It.IsAny<byte[]>())).Callback<IdxKey, byte[]>((k, v) =>
+            {
+                Assert.AreEqual(StreamKey("Snapshot:588", true, 8), k, "Key");
+                AssertExtension.AreEqual(LongToBytes(96674), v, "Value");
+            }).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            index.AddSnapshot("Snapshot:588", 8, 96674);
+            _streams.Verify();
+        }
+
+        [TestMethod]
+        public void FindAllEvents()
+        {
+            var minKey = StreamKey("Agg:5957", false, 0);
+            var maxKey = StreamKey("Agg:5957", false, int.MaxValue);
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            var expectedPositions = new List<long>();
+            for (int i = 1; i <= 17; i++)
+            {
+                var key = StreamKey("Agg:5957", false, i);
+                var position = 2847 + 68 * i;
+                var value = LongToBytes(position);
+                result.Add(new KeyValuePair<IdxKey, byte[]>(key, value));
+                expectedPositions.Add(position);
+            }
+            _streams.Setup(s => s.Select(minKey, maxKey)).Returns(result).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            IEnumerable<long> eventPositions = index.FindEvents("Agg:5957", 0);
+            _streams.Verify();
+            AssertExtension.AreEqual(expectedPositions.ToArray(), eventPositions.ToArray());
+        }
+
+        [TestMethod]
+        public void FindSnapshot()
+        {
+            var minKey = StreamKey("Agg:5957", true, 0);
+            var maxKey = StreamKey("Agg:5957", true, int.MaxValue);
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            result.Add(new KeyValuePair<IdxKey, byte[]>(StreamKey("Agg:5957", true, 4), LongToBytes(29957)));
+            result.Add(new KeyValuePair<IdxKey, byte[]>(StreamKey("Agg:5957", true, 9), LongToBytes(52117)));
+            result.Add(new KeyValuePair<IdxKey, byte[]>(StreamKey("Agg:5957", true, 15), LongToBytes(68521)));
+            _streams.Setup(s => s.Select(minKey, maxKey)).Returns(result).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            long snapshotPosition = index.FindSnapshot("Agg:5957");
+            _streams.Verify();
+            Assert.AreEqual(68521, snapshotPosition);
+        }
+
+        [TestMethod]
+        public void FindEventsFromVersion()
+        {
+            var minKey = StreamKey("Agg:5957", false, 8);
+            var maxKey = StreamKey("Agg:5957", false, int.MaxValue);
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            var expectedPositions = new List<long>();
+            for (int i = 8; i <= 17; i++)
+            {
+                var key = StreamKey("Agg:5957", false, i);
+                var position = 2847 + 68 * i;
+                var value = LongToBytes(position);
+                result.Add(new KeyValuePair<IdxKey, byte[]>(key, value));
+                expectedPositions.Add(position);
+            }
+            _streams.Setup(s => s.Select(minKey, maxKey)).Returns(result).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            IEnumerable<long> eventPositions = index.FindEvents("Agg:5957", 8);
+            _streams.Verify();
+            AssertExtension.AreEqual(expectedPositions.ToArray(), eventPositions.ToArray());
+        }
+
+        [TestMethod]
+        public void CheckThatStreamDoesNotExist()
+        {
+            var minKey = StreamKey("Aggregate:5957", false, 0);
+            var maxKey = StreamKey("Aggregate:5957", false, 1);
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            _streams.Setup(s => s.Select(minKey, maxKey)).Returns(result).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            var streamExists = index.StreamExists("Aggregate:5957");
+            _streams.Verify();
+            Assert.IsFalse(streamExists);
+        }
+
+        [TestMethod]
+        public void CheckThatStreamExists()
+        {
+            var minKey = StreamKey("Aggregate:5957", false, 0);
+            var maxKey = StreamKey("Aggregate:5957", false, 1);
+            var result = new List<KeyValuePair<IdxKey, byte[]>>();
+            result.Add(new KeyValuePair<IdxKey,byte[]>(StreamKey("Aggregate:5957", false, 1), LongToBytes(5844)));
+            _streams.Setup(s => s.Select(minKey, maxKey)).Returns(result).Verifiable();
+            var index = new FileEventStoreIndexCore(_headers.Object, _streams.Object);
+            var streamExists = index.StreamExists("Aggregate:5957");
+            _streams.Verify();
+            Assert.IsTrue(streamExists);
+        }
+    }
+}
---
+++ CqrsFramework/EventStore/FileEventStoreIndexCore.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsFramework.IndexTable;
+
+namespace CqrsFramework.EventStore
+{
+    public class FileEventStoreIndexCore
+    {
+        private IIdxTree _headers;
+        private IIdxTree _streams;
+
+        private long _appendPosition = 0;
+        private long _unpublishedPosition = 0;
+        private IdxKey _appendKey = IdxKey.FromBytes(new byte[1] { 0 });
+        private IdxKey _unpublishedKey = IdxKey.FromBytes(new byte[1] { 1 });
+        private bool _appendExists = false;
+        private bool _unpublishedExists = false;
+        private Encoding _keyEncoding = Encoding.ASCII;
+
+        public FileEventStoreIndexCore(IIdxTree headers, IIdxTree streams)
+        {
+            this._headers = headers;
+            this._streams = streams;
+
+            LoadHeaders();
+        }
+
+        public long UnpublishedPosition
+        {
+            get { return _unpublishedPosition; }
+            set { _unpublishedPosition = value; }
+        }
+        public long AppendPosition
+        {
+            get { return _appendPosition; }
+            set { _appendPosition = value; }
+        }
+
+        public void Flush()
+        {
+            if (_appendExists)
+                _headers.Update(_appendKey, WriteLong(_appendPosition));
+            else
+                _headers.Insert(_appendKey, WriteLong(_appendPosition));
+            if (_unpublishedExists)
+                _headers.Update(_unpublishedKey, WriteLong(_unpublishedPosition));
+            else
+                _headers.Insert(_unpublishedKey, WriteLong(_unpublishedPosition));
+        }
+
+        private void LoadHeaders()
+        {
+            foreach (var pair in _headers.Select(_appendKey, _unpublishedKey))
+            {
+                if (pair.Key == _appendKey)
+                {
+                    _appendPosition = ReadLong(pair.Value.ToArray());
+                    _appendExists = true;
+                }
+                else if (pair.Key == _unpublishedKey)
+                {
+                    _unpublishedPosition = ReadLong(pair.Value.ToArray());
+                    _unpublishedExists = true;
+                }
+            }
+        }
+
+        private long ReadLong(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return BitConverter.ToInt64(bytes, 0);
+        }
+
+        private byte[] WriteLong(long value)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return bytes;
+        }
+
+        private IdxKey StreamKey(string streamId, bool snapshot, int version)
+        {
+            var stringByteCount = (short)_keyEncoding.GetByteCount(streamId);
+            var bytes = new byte[2 + stringByteCount + 5];
+            bytes[0] = (byte)((stringByteCount >> 8) & 0xff);
+            bytes[1] = (byte)((stringByteCount) & 0xff);
+            _keyEncoding.GetBytes(streamId, 0, streamId.Length, bytes, 2);
+            var typeOffset = 2 + stringByteCount;
+            bytes[typeOffset] = snapshot ? (byte)0 : (byte)1;
+            var versionOffset = typeOffset + 1;
+            bytes[versionOffset + 0] = (byte)((version >> 24) & 0xff);
+            bytes[versionOffset + 1] = (byte)((version >> 16) & 0xff);
+            bytes[versionOffset + 2] = (byte)((version >> 8) & 0xff);
+            bytes[versionOffset + 3] = (byte)((version) & 0xff);
+            return IdxKey.FromBytes(bytes);
+        }
+
+        public void AddRecord(string streamKey, int version, long position)
+        {
+            var key = StreamKey(streamKey, false, version);
+            var value = WriteLong(position);
+            _streams.Insert(key, value);
+        }
+
+        public void AddSnapshot(string streamKey, int version, long position)
+        {
+            var key = StreamKey(streamKey, true, version);
+            var value = WriteLong(position);
+            _streams.Insert(key, value);
+        }
+
+        private IEnumerable<long> FindInStreams(IdxKey minKey, IdxKey maxKey)
+        {
+            var pairs = _streams.Select(minKey, maxKey);
+            return pairs.Select(v => ReadLong(v.Value));
+        }
+
+        public IEnumerable<long> FindEvents(string streamKey, int minVersion)
+        {
+            return FindInStreams(StreamKey(streamKey, false, minVersion), StreamKey(streamKey, false, int.MaxValue));
+        }
+
+        public long FindSnapshot(string streamKey)
+        {
+            return FindInStreams(StreamKey(streamKey, true, 0), StreamKey(streamKey, true, int.MaxValue)).LastOrDefault();
+        }
+
+        public bool StreamExists(string streamKey)
+        {
+            return FindInStreams(StreamKey(streamKey, false, 0), StreamKey(streamKey, false, 1)).Any();
+        }
+    }
+}
