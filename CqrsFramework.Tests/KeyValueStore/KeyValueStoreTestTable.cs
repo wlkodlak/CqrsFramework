d3f63c07e21dbacee42366bb57fe6548943d8bec
Milan Wilczak <wlkodlak@seznam.cz>
2013-06-25 22:52:24 +0200

    TableKeyValueStore

---
+++ CqrsFramework.Tests/KeyValueStore/KeyValueStoreTestTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsFramework.KeyValueStore;
+using CqrsFramework.Infrastructure;
+using System.Data;
+
+namespace CqrsFramework.Tests.KeyValueStore
+{
+    [TestClass]
+    public class KeyValueStoreTestTable : KeyValueStoreTestBase
+    {
+        protected override IBuildStore CreateBuilder()
+        {
+            return new Builder();
+        }
+
+        private class Builder : IBuildStore
+        {
+            private DataTable _table;
+            private MemoryTableProvider _provider;
+
+            public Builder()
+            {
+                _table = new DataTable("KeyValueStore");
+                _table.Columns.Add("id", typeof(int));
+                _table.Columns.Add("key", typeof(string));
+                _table.Columns.Add("version", typeof(int));
+                _table.Columns.Add("data", typeof(byte[]));
+                _provider = new MemoryTableProvider(_table, null);
+            }
+
+            public void With(KeyValueDocument document)
+            {
+                var row = _provider.NewRow();
+                row["key"] = document.Key;
+                row["version"] = document.Version;
+                row["data"] = document.Data;
+                _provider.Insert(row);
+            }
+
+            public List<KeyValueDocument> GetAll()
+            {
+                return _provider.GetRows().Select(row => new KeyValueDocument(row.Get<string>("key"), row.Get<int>("version"), row.Get<byte[]>("data"))).ToList();
+            }
+
+            public IKeyValueStore Build()
+            {
+                return new TableKeyValueStore(_provider);
+            }
+        }
+    }
+}
---
+++ CqrsFramework/KeyValueStore/TableKeyValueStore.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsFramework.Infrastructure;
+
+namespace CqrsFramework.KeyValueStore
+{
+    public class TableKeyValueStore : IKeyValueStore
+    {
+        private ITableProvider _provider;
+
+        public TableKeyValueStore(ITableProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public KeyValueDocument Get(string key)
+        {
+            var row = _provider.GetRows().Where("key").Is(key).FirstOrDefault();
+            return row == null ? null : new KeyValueDocument(row.Get<string>("key"), row.Get<int>("version"), row.Get<byte[]>("data"));
+        }
+
+        public int Set(string key, int expectedVersion, byte[] data)
+        {
+            var row = _provider.GetRows().Where("key").Is(key).FirstOrDefault();
+            var version = row == null ? 0 : row.Get<int>("version");
+            VerifyVersion(key, expectedVersion, version);
+            if (row == null)
+            {
+                row = _provider.NewRow();
+                row["key"] = key;
+                row["version"] = 1;
+                row["data"] = data;
+                _provider.Insert(row);
+                return 1;
+            }
+            else
+            {
+                row["version"] = version + 1;
+                row["data"] = data;
+                _provider.Update(row);
+                return version + 1;
+            }
+        }
+
+        private void VerifyVersion(string key, int expected, int actual)
+        {
+            if (expected != -1 && expected != actual)
+                throw KeyValueStoreException.BadVersion(key, expected, actual);
+        }
+
+        public IEnumerable<string> Enumerate()
+        {
+            return _provider.GetRows().Select(row => row.Get<string>("key")).ToList();
+        }
+
+        public void Flush()
+        {
+        }
+
+        public void Purge()
+        {
+            _provider.GetRows().ToList().ForEach(_provider.Delete);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
